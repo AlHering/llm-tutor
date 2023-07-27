ba316f0ca4c82f48f4f18499805fa3faa86dd12e
Alexander Hering <dev.hering@gmail.com>
2023-07-27 16:39:50 +0200

    added folder handling and preprocessing to kb

--- src/control/knowledgebase_controller.py
+++ src/control/knowledgebase_controller.py
@@ -6,14 +6,30 @@
 ****************************************************
 """
 import os
-from typing import Any, List
-import chromadb
+from typing import Any, List, Tuple
 from chromadb.api.types import EmbeddingFunction, Embeddings
 from chromadb.config import Settings
 from langchain.vectorstores import Chroma
 from langchain.docstore.document import Document
 from langchain.vectorstores.base import VectorStoreRetriever
 from src.utility.bronze.hashing_utility import hash_text_with_sha256
+from langchain.chains import RetrievalQA
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from multiprocessing import Pool
+from tqdm import tqdm
+from src.utility import langchain_utility
+
+
+def reload_document(document_path: str) -> Document:
+    """
+    Function for (re)loading document content.
+    :param document_path: Document path.
+    :return: Document object.
+    """
+    res = langchain_utility.DOCUMENT_LOADERS[os.path.splitext(document_path)[
+        1]](document_path).load()
+    return res[0] if isinstance(res, list) and len(res) == 1 else res
+
 
 class KnowledgeBaseController(object):
     """
@@ -80,3 +96,40 @@ class KnowledgeBaseController(object):
         :param ids: Custom IDs to add. Defaults to the hash of the document contents.
         """
         self.clients.get(name, self.clients["base"]).add_documents(documents=documents, ids=[[hash_text_with_sha256(document.page_content) for document in documents]] if ids is None else ids)
+
+    def load_folder(self, folder: str, target_client: str = "base", splitting: Tuple[int] = None) -> None:
+        """
+        Method for (re)loading folder contents.
+        :param folder: Folder path.
+        :param target_client: Client/collection to handle folder contents. Defaults to "base".
+        :param splitting: A tuple of chunk size and overlap for splitting. Defaults to None in which case the documents are not split.
+        """
+        document_paths = []
+        documents = []
+        for root, dirs, files in os.walk(folder, topdown=True):
+            document_paths.extend([os.path.join(root, file) for file in files if any(file.lower().endswith(
+                supported_extension) for supported_extension in langchain_utility.DOCUMENT_LOADERS)])
+
+        with Pool(processes=os.cpu_count()) as pool:
+            with tqdm(total=len(document_paths), desc="(Re)loading folder contents...", ncols=80) as progress_bar:
+                for index, loaded_document in enumerate(pool.imap(reload_document, document_paths)):
+                    documents.append(loaded_document)
+                    progress_bar.update(index)
+
+        if splitting is not None:
+            documents = self.split_documents(documents, *splitting)
+        
+        self.embed_documents(target_client, documents)
+
+    def split_documents(self, documents: List[Document], split: int, overlap: int) -> List[Document]:
+        """
+        Method for splitting document content.
+        :param documents: Documents to split.
+        :param split: Chunk size to split documents into.
+        :param overlap: Overlap for split chunks.
+        :return: Split documents.
+        """
+        return RecursiveCharacterTextSplitter(
+            chunk_size=self.profile["splitting_chunks"],
+            chunk_overlap=self.profile["splitting_overlap"],
+            length_function=len).split_documents(documents)
