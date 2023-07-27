f3f5caa077b6c79e1bee0c7c9de43ab5d5767154
Alexander Hering <dev.hering@gmail.com>
2023-07-27 12:40:11 +0200

    starting frontend

---
+++ src/view/app.py
@@ -0,0 +1,17 @@
+
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*                    LLM Tutor                     *
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+import streamlit as st
+
+
+
+def run_app() -> None:
+    """
+    Function for running app.
+    """
+    pass
