28aa04be09dda26115deff467765aecc449e15c0
Alexander Hering <dev.hering@gmail.com>
2023-07-27 13:45:50 +0200

    added text hashing

--- src/utility/bronze/hashing_utility.py
+++ src/utility/bronze/hashing_utility.py
@@ -21,3 +21,14 @@ def hash_with_sha256(file_path: str) -> str:
         for n in iter(lambda : f.readinto(mv), 0):
             h.update(mv[:n])
     return h.hexdigest()
+
+
+def hash_text_with_sha256(text: str) -> str:
+    """
+    Function for hashing text with SHA256.
+    :param text: Text to hash.
+    :return: Hash.
+    """
+    h = hashlib.sha256()
+    h.update(bytes(text, "utf-8"))
+    return h.hexdigest()
