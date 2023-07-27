ba41aaba52a98d9e02055264e5c11ac51320de32
Alexander Hering <dev.hering@gmail.com>
2023-07-27 19:17:14 +0200

    organized and added model path

--- src/configuration/paths.py
+++ src/configuration/paths.py
@@ -7,6 +7,9 @@
 """
 import os
 
+"""
+Base 
+"""
 PACKAGE_PATH = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 SOURCE_PATH = os.path.join(PACKAGE_PATH, "src")
@@ -15,7 +18,14 @@ SUBMODULE_PATH = os.path.join(SOURCE_PATH, "submodules")
 DATA_PATH = os.path.join(PACKAGE_PATH, "data")
 PLUGIN_PATH = os.path.join(SOURCE_PATH, "plugins")
 DUMP_PATH = os.path.join(DATA_PATH, "processes", "dumps")
+
+
+"""
+Machine Learning Models
+"""
 TEXTGENERATION_MODEL_PATH = os.path.join(
     PACKAGE_PATH, "machine_learning_models", "MODELS")
 TEXTGENERATION_LORA_PATH = os.path.join(
     PACKAGE_PATH, "machine_learning_models", "LORAS")
+E5_LARGE_V3_PATH = os.path.join(
+    TEXTGENERATION_MODEL_PATH, "intfloat_e5-large-v2")
