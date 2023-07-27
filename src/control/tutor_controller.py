9c29c8f29f03d264c1904212434351764fad49a0
Alexander Hering <dev.hering@gmail.com>
2023-07-27 17:05:24 +0200

    refactored tutor controller abstraction

--- src/control/tutor_controller.py
+++ src/control/tutor_controller.py
@@ -28,6 +28,9 @@ class TutorController(object):
         self.llm_type = None
         self.interface = None
         self.knowledge_base = None
+        self.doc_types = {
+            "base": {"splitting": None}
+        }
 
     def load_general_llm(self, model_path: str, model_type: str) -> None:
         """
@@ -46,23 +49,33 @@ class TutorController(object):
         :param kb_base_embedding_function: Base embedding function to use for knowledgebase.
         """
         self.knowledge_base = KnowledgeBaseController(peristant_directory=kb_path, base_embedding_function=kb_base_embedding_function)
+
+    def register_document_type(self, documen_type: str, embedding_function: EmbeddingFunction = None, splitting: Tuple[int] = None) -> None:
+        """
+        Method for registering a document type.
+        :param documen_type: Name to identify the documen type.
+        :param embedding_function: Embedding function the document type. Defaults to base embedding function.
+        :param splitting: A tuple of chunk size and overlap for splitting. Defaults to None in which case the documents are not split.
+        """
+        self.doc_types[documen_type] = {"splitting": splitting}
+        self.knowledge_base.get_or_create_client(documen_type, embedding_function)
+
         
-    def load_documents(self, documents: List[Document], target_client: str = "base") -> None:
+    def load_documents(self, documents: List[Document], document_type: str = "base") -> None:
         """
         Method for loading documents into knowledgebase.
         :param documents: Documents to load.
-        :param target_client: Client/collection to handle folder contents. Defaults to "base".
+        :param document_type: Name to identify the documen type. Defaults to "base".
         """
-        self.knowledge_base.embed_documents(name=target_client, documents=documents)
+        self.knowledge_base.embed_documents(name=document_type, documents=documents)
 
-    def load_files(self, file_paths: List[str], target_client: str = "base", splitting: Tuple[int] = None) -> None:
+    def load_files(self, file_paths: List[str], document_type: str = "base") -> None:
         """
         Method for (re)loading folder contents.
         :param folder: Folder path.
-        :param target_client: Client/collection to handle folder contents. Defaults to "base".
-        :param splitting: A tuple of chunk size and overlap for splitting. Defaults to None in which case the documents are not split.
+        :param document_type: Name to identify the documen type. Defaults to "base".
         """
-        self.knowledge_base.load_files(file_paths, target_client, splitting)
+        self.knowledge_base.load_files(file_paths, document_type, self.doc_types.get(document_type, {}).get("splitting"))
 
     def start_conversation(self):
         """
