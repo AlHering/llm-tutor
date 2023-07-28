d4687f6125de4b930d437352567b3bfc2df86c0f
Alexander Hering <dev.hering@gmail.com>
2023-07-28 15:00:04 +0200

    adding handle for starting backend

---
+++ start_backend.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*                    LLM Tutor                     *
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+import os
+from src.interfaces import backend_interface
+
+
+if __name__ == "__main__":
+    backend_interface.run_backend()
