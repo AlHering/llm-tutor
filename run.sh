a0053e3dd2e2d79150fcdc0115c10d68bd0131f2
Alexander Hering <dev.hering@gmail.com>
2023-07-28 10:06:36 +0200

    implemented runner

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ cd "/project"
 printf "\n%s\n" "${delimiter}"
 printf "Activating conda environment and entering __main__.py"
 printf "\n%s\n" "${delimiter}"
-source "${CONDA_DIR}/etc/profile.d/conda.sh" && python __main__.py
+source "${CONDA_DIR}/etc/profile.d/conda.sh" && streamlit run __main__.py --server.port 7860
