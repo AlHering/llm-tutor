6fa00a26ec79700e22851f70b9147116266c8641
Alexander Hering <dev.hering@gmail.com>
2023-07-30 18:57:00 +0200

    extracted interfacing functionality

--- src/view/streamlit_frontend/app.py
+++ src/view/streamlit_frontend/app.py
@@ -11,106 +11,27 @@ from typing import Any
 import streamlit as st
 from src.configuration import configuration as cfg
 from src.view.streamlit_frontend.chat_template import css, bot_template, user_template
+from src.utility.bronze import streamlit_utility
+from src.interfaces. streamlit_interface import start_controller_if_stopped, handle_request
 import json
 import requests
-
-
-BACKEND_BASE_URL = f"http://{cfg.BACKEND_HOST}:{cfg.BACKEND_PORT}"
-
-
-def handle_user_question(question: str) -> str:
-    """
-    Function for handling user question.
-    :param query: User question.
-    :return: Answer.
-    """
-    pass
-
-
-def handle_user_conversation(conversation_uuid: str, question: str) -> dict:
-    """
-    Function for handling user conversation.
-    :param conversation_uuid: Conversation UUID.
-    :param question: User question.
-    :return: Conversation result.
-    """
-    pass
-
-
-def handle_request(method: str, endpoint: str, data: dict = None) -> Any:
-    """
-    Function for handling requests.
-    :param method: Request method ("get", "post").
-    :param endpoint: Endpoint to send request to.
-    :param data: JSON data to include in request. Defaults to None.
-    :return: Extracted response content.
-    """
-    print(f"{method} :: {endpoint} :: {data}")
-    try:
-        response = {"get": requests.get, "post": requests.post}[
-            method](f"{BACKEND_BASE_URL}{endpoint}", json=data)
-
-    except requests.exceptions.SSLError:
-        print("SSL Error")
-        response = {"get": requests.get, "post": requests.post}[
-            method](f"{BACKEND_BASE_URL}{endpoint}", json=data, verify=False)
-    try:
-        res = json.loads(response.content.decode('utf-8'))
-    except json.decoder.JSONDecodeError:
-        res = response.text
-    print(res)
-    return res
 
 
 def run_app() -> None:
     """
     Function for running app.
     """
-    resp = handle_request("get", "/")
-    if resp["message"] == "System is stopped":
-        resp = handle_request("post", "/start")
-        print(resp)
-        resp = handle_request("post", "/load_llm", {
-            "model_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
-                                       "TheBloke_orca_mini_7B-GGML/orca-mini-7b.ggmlv3.q4_1.bin"),
-            "model_type": "llamacpp"
-        })
-        print(resp)
-        resp = handle_request("post", "/load_kb")
-        print(resp)
-
     st.set_page_config(
         page_title="LLM Tutor",
         page_icon=":books:"
     )
+    st.title("Index")
     st.write(css, unsafe_allow_html=True)
 
     st.header("LLM Tutor: Upload and talk to learning material.")
-    st.text_input("Query:")
-
-    st.write(user_template.replace("{{MSG}}", "Test"), unsafe_allow_html=True)
-    st.write(bot_template.replace("{{MSG}}", "Test"), unsafe_allow_html=True)
-
-    with st.sidebar:
-        st.subheader("Learning Material")
-        files = {
-            "skripts": st.file_uploader("Skripts", accept_multiple_files=True),
-            "presentations": st.file_uploader("Presentations", accept_multiple_files=True),
-            "books": st.file_uploader("Books", accept_multiple_files=True)
-        }
-        if st.button("Load"):
-            with st.spinner("Processing"):
-                for doc_type in files:
-                    if files[doc_type]:
-                        data = {"documents": [
-                                {"page_content": f.read().decode("utf-8"),
-                                 "metadata": {"file_name": f.name, "file_size": f.size, "file_type": f.type},
-                                 "collection": doc_type
-                                 }
-                                for f in files[doc_type]]}
-                        resp = handle_request("post", "/embed", data=data)
-                        print(resp)
-                        files[doc_type].clear()
+    if "backend_controller_started" not in st.session_state:
+        start_controller_if_stopped()
+        st.session_state["backend_controller_started"] = True
 
 
 if __name__ == "__main__":
