cf27e04c54ddacec31d0ddb922606cb6034cfb95
Alexander Hering <dev.hering@gmail.com>
2023-07-30 18:57:19 +0200

    adding page for knowledgebase handling

---
+++ src/view/streamlit_frontend/pages/knowledgebase_app.py
@@ -0,0 +1,31 @@
+
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*                    LLM Tutor                     *
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+import os
+from typing import Any
+from time import sleep
+import streamlit as st
+from src.configuration import configuration as cfg
+from src.utility.bronze import streamlit_utility
+from src.view.streamlit_frontend.chat_template import css, bot_template, user_template
+from src.interfaces. streamlit_interface import handle_request
+import json
+import requests
+
+
+st.set_page_config(
+    page_title="LLM Tutor",
+    page_icon=":books:"
+)
+st.title("Knowledgebases")
+st.write(css, unsafe_allow_html=True)
+
+st.header("Knowlegebase Control")
+
+streamlit_utility.wait_for_state_variable(
+    streamlit_context=st, variable_name="backend_controller_started", waiting_message="Waiting for controller setup...")
