e7f1d67f82f32ddb7a7e8f262518763d6d71933b
Alexander Hering <dev.hering@gmail.com>
2023-07-30 16:01:28 +0200

    fixed path cleaning

--- src/configuration/paths.py
+++ src/configuration/paths.py
@@ -10,8 +10,8 @@ import os
 """
 Base 
 """
-PACKAGE_PATH = os.path.dirname(os.path.dirname(
-    os.path.dirname(os.path.abspath(__file__))))
+PACKAGE_PATH = os.path.abspath(os.path.dirname(os.path.dirname(
+    os.path.dirname(__file__))))
 SOURCE_PATH = os.path.join(PACKAGE_PATH, "src")
 DOCS_PATH = os.path.join(PACKAGE_PATH, "docs")
 SUBMODULE_PATH = os.path.join(SOURCE_PATH, "submodules")
