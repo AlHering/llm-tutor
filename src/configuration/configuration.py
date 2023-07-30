4ec11c02ba822216d98fb0078b6b2747f8955aa4
Alexander Hering <dev.hering@gmail.com>
2023-07-30 16:01:59 +0200

    setting up flask config in global config

--- src/configuration/configuration.py
+++ src/configuration/configuration.py
@@ -10,6 +10,7 @@ import logging
 from dotenv import dotenv_values
 from . import paths as PATHS
 from . import urls as URLS
+from . import flask_frontend_config
 
 
 """
@@ -17,7 +18,18 @@ Environment file
 """
 ENV = dotenv_values(os.path.join(PATHS.PACKAGE_PATH, ".env"))
 
+
 """
 Logger
 """
 LOGGER = logging.Logger("LLMTutor")
+
+
+"""
+FRONTENDS
+"""
+FLASK_CONFIG = flask_frontend_config.global_config
+PATHS.FLASK_COMMON_STATIC = os.path.join(
+    PATHS.SOURCE_PATH, "view", "flask_frontend", "common_static")
+PATHS.FLASK_COMMON_TEMPLATES = os.path.join(
+    PATHS.SOURCE_PATH, "view", "flask_frontend", "common_templates")
