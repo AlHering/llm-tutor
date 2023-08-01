8d1e26d2245aaf98f04cd55e8f124563038b4e7d
Alexander Hering <dev.hering@gmail.com>
2023-08-01 15:07:39 +0200

    cleared endpoint functions

--- src/interfaces/backend_interface.py
+++ src/interfaces/backend_interface.py
@@ -128,8 +128,8 @@ class Endpoints(str, Enum):
         return self.value
 
 
-@BACKEND.get(Endpoints.GET_ROOT)
-def get_status() -> dict:
+@BACKEND.get(Endpoints.GET_STATUS)
+async def get_status() -> dict:
     """
     Root endpoint for getting system status.
     :return: Response.
@@ -140,173 +140,15 @@ def get_status() -> dict:
 
 
 @BACKEND.post(Endpoints.POST_START)
-def post_start() -> dict:
+async def post_start() -> dict:
     """
     Endpoint for starting system.
     :return: Response.
     """
     global STARTED
-    global CONTROLLER
     if STARTED == False:
-        CONTROLLER = TutorController()
         STARTED = True
     return {"message": f"System is {'started' if STARTED else 'stopped'}"}
-
-
-@BACKEND.get(Endpoints.GET_CONFIGS)
-def get_configs() -> dict:
-    """
-    Endpoint retrieving available configs.
-    :return: Response.
-    """
-    global STARTED
-    global CONTROLLER
-    if STARTED == True:
-        return {"configs": CONTROLLER.get_available_configs()}
-    else:
-        return {"message": "System is stopped!"}
-
-
-@BACKEND.post(Endpoints.POST_SAVE_CONFIG)
-def post_save_config(config_name: str) -> dict:
-    """
-    Endpoint for saving config.
-    :param config_name: Name to save config under.
-    :return: Response.
-    """
-    global STARTED
-    global CONTROLLER
-    if STARTED == True:
-        CONTROLLER.save_config(config_name)
-        return {"message": f"Config '{config_name}' saved."}
-    else:
-        return {"message": "System is stopped!"}
-
-
-@BACKEND.post(Endpoints.POST_LOAD_CONFIG)
-def post_load_config(config_name: str) -> dict:
-    """
-    Endpoint for loading config.
-    :param config_name: Name of config to load.
-    :return: Response.
-    """
-    global STARTED
-    global CONTROLLER
-    if STARTED == True:
-        CONTROLLER.load_config(config_name)
-        return {"message": f"Config '{config_name}' loaded."}
-    else:
-        return {"message": "System is stopped!"}
-
-
-@BACKEND.post(Endpoints.POST_LOAD_LLM)
-def post_load_llm(llm_description: LLMDescription) -> dict:
-    """
-    Endpoint for loading models.
-    :param llm_description: LLM description configuring loading parameters.
-    :return: Response.
-    """
-    global STARTED
-    global CONTROLLER
-    if STARTED == True:
-        CONTROLLER.load_general_llm(**llm_description.dict())
-        return {"message": "LLM loaded."}
-    else:
-        return {"message": "System is stopped!"}
-
-
-@BACKEND.post(Endpoints.POST_LOAD_KB)
-def post_load_kb() -> dict:
-    """
-    Endpoint for loading a knowledge base.
-    :return: Response.
-    """
-    global STARTED
-    global CONTROLLER
-    if STARTED == True:
-        CONTROLLER.load_knowledge_base(os.path.join(
-            cfg.PATHS.DATA_PATH, "backend", "kb"))
-        return {"message": "Knowledgebase loaded."}
-    else:
-        return {"message": "System is stopped!"}
-
-
-@BACKEND.post(Endpoints.POST_EMBED)
-def post_embed(docs: DocumentList, collection: str = None) -> dict:
-    """
-    Endpoint for embedding a list of documents.
-    :param docs: List of documents to embed.
-    :param collection: Collection to embed documents in. Defaults to None in which case default the collection is set.
-    :return: Response.
-    """
-    global STARTED
-    global CONTROLLER
-    if STARTED == True:
-        if CONTROLLER.kb is not None:
-            CONTROLLER.load_documents(docs.documents, collection)
-            return {"message": "Documents loaded."}
-        else:
-            return {"message": "No knowledgebase loaded!"}
-    else:
-        return {"message": "System is stopped!"}
-
-
-@BACKEND.post(Endpoints.POST_START_CONVERSATION)
-def post_start_conversation(conversation_uuid: str = None, collection: str = None) -> dict:
-    """
-    Endpoint for starting conversation.
-    :param conversation_uuid: UUID to start conversation with. Defaults to None in which case a UUID is generated.
-    :param collection: Collection to embed documents in. Defaults to None in which case default the collection is set.
-    :return: Response.
-    """
-    global STARTED
-    global CONTROLLER
-    if STARTED == True:
-        if CONTROLLER.kb is not None:
-            return {"conversation": CONTROLLER.start_conversation(conversation_uuid, collection)}
-        else:
-            return {"message": "No knowledgebase loaded!"}
-    else:
-        return {"message": "System is stopped!"}
-
-
-@BACKEND.post(Endpoints.POST_CONVERSATION_QUERY)
-def post_conversation_query(query: str, conversation_uuid: str = None) -> dict:
-    """
-    Endpoint for sending query into conversation.
-    :param query: Query to send.
-    :param conversation_uuid: UUID of conversation to send query to. Defaults to None in which case a new conversation ist started.
-    :return: Response.
-    """
-    global STARTED
-    global CONTROLLER
-    if STARTED == True:
-        if CONTROLLER.kb is not None:
-            return {"result": CONTROLLER.conversational_query(conversation_uuid, query)}
-        else:
-            return {"message": "No knowledgebase loaded!"}
-    else:
-        return {"message": "System is stopped!"}
-
-
-@BACKEND.post(Endpoints.POST_QUERY)
-def post_query(query: str, collection: str = None, include_source: bool = True) -> dict:
-    """
-    Endpoint for querying knowledgebase.
-    :param query: Query to send.
-    :param collection: Collection to embed documents in. Defaults to None in which case default the collection is set.
-    :param include_source: Flag for declaring whether to include source. Defaults to True.
-    :return: Response.
-    """
-    global STARTED
-    global CONTROLLER
-    if STARTED == True:
-        if CONTROLLER.kb is not None:
-            return {"result": CONTROLLER.query(query, collection, include_source)}
-        else:
-            return {"message": "No knowledgebase loaded!"}
-    else:
-        return {"message": "System is stopped!"}
 
 
 """
