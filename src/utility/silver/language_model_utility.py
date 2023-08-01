d14d9fb012ce5bacd1db1b3c8ae23472ae5fbc60
Alexander Hering <dev.hering@gmail.com>
2023-08-01 22:59:27 +0200

    starting language model utility

---
+++ src/utility/silver/language_model_utility.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*                     Utility                      *
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+from typing import Any
+
+
+def spawn_language_model_instance(config: str) -> Any:
+    """
+    Function for spawning language model instance based on configuration.
+    :param config: Instance configuration.
+    :return: Language model instance.
+    """
+    pass
