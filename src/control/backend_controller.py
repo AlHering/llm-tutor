6e9a3eac4367cbc53ac20244e8bf92764a8bfc85
Alexander Hering <dev.hering@gmail.com>
2023-09-07 19:41:26 +0200

    streamlined control strucutre

--- src/control/backend_controller.py
+++ src/control/backend_controller.py
@@ -6,17 +6,20 @@
 ****************************************************
 """
 import os
-from uuid import UUID
+from uuid import UUID, uuid4
 from time import sleep
 from datetime import datetime as dt
-from typing import Optional, Any, List
+from typing import Optional, Any, List, Dict
 from src.configuration import configuration as cfg
 from src.utility.gold.filter_mask import FilterMask
 from src.utility.bronze import sqlalchemy_utility
-from src.control.knowledgebase_controller import KnowledgeBaseController
 from src.utility.bronze.hashing_utility import hash_text_with_sha256
 from src.model.backend_control.data_model import populate_data_instrastructure
 from src.model.backend_control.llm_pool import ThreadedLLMPool
+from src.utility.silver import embedding_utility
+from src.utility.bronze.hashing_utility import hash_text_with_sha256
+from src.utility.silver.file_system_utility import safely_create_path
+from src.model.knowledgebase_control.chromadb_knowledgebase import ChromaKnowledgeBase, KnowledgeBase
 
 
 class BackendController(object):
@@ -49,12 +52,16 @@ class BackendController(object):
         self._setup_database()
 
         # Knowledgebase infrastructure
-        self.kb_controller = KnowledgeBaseController(
-            working_directory=os.path.join(
-                self.working_directory, "knowledgebases"),
-            kb_configs=[
-                entry.config for entry in self.get_objects_by_type("kb_config")]
+        self.document_directory = os.path.join(
+            self.working_directory, "library")
+        safely_create_path(self.document_directory)
+        self.default_embedding_function = embedding_utility.LocalHuggingFaceEmbeddings(
+            cfg.PATHS.INSTRUCT_XL_PATH
         )
+        self.kbs: Dict[str, KnowledgeBase] = {}
+        self.documents = {}
+        for kb_config in self.get_objects_by_type("kb_config"):
+            self.register_knowledgebase(kb_config)
 
         # LLM infrastructure
         self.llm_pool = ThreadedLLMPool()
@@ -112,7 +119,7 @@ class BackendController(object):
             sleep(2.0)
 
     """
-    LLM Handling methods
+    LLM handling methods
     """
 
     def load_instance(self, instance_uuid: str) -> Optional[str]:
@@ -171,6 +178,87 @@ class BackendController(object):
         """
         self.load_instance(instance_uuid)
         return self.llm_pool.generate(instance_uuid, prompt)
+
+    """
+    Knowledgebase handling methods
+    """
+
+    def register_knowledgebase(self, config: dict) -> str:
+        """
+        Method for registering knowledgebase.
+        :param config: Knowledgebase config.
+        :return: Knowledgebase name.
+        """
+        name = config.get("name", uuid4())
+        handler = config.get("handler", "chromadb")
+        handler_kwargs = config.get("handler_kwargs", {
+            "peristant_directory": os.path.join(self.working_directory, name),
+            "base_embedding_function": self.default_embedding_function,
+            "implementation": "duckdb+parquet"}
+        )
+
+        self.kbs[name] = {"chromadb": ChromaKnowledgeBase}[handler](
+            **handler_kwargs
+        )
+        return name
+
+    def delete_documents(self, kb: str, document_ids: List[Any], collection: str = "base") -> None:
+        """
+        Method for deleting a document from the knowledgebase.
+        :param kb: Target knowledgebase.
+        :param document_ids: Document IDs.
+        :param collection: Collection to remove document from.
+        """
+        for document_id in document_ids:
+            self.kbs[kb].delete_document(document_id, collection)
+
+    def wipe_knowledgebase(self, target_kb: str) -> None:
+        """
+        Method for wiping a knowledgebase.
+        :param target_kb: Target knowledgebase.
+        """
+        self.kbs[target_kb].wipe_knowledgebase()
+
+    def migrate_knowledgebase(self, source_kb: str, target_kb: str) -> None:
+        """
+        Method for migrating knowledgebase.
+        :param source_kb: Source knowledgebase.
+        :param target_kb: Target knowledgebase.
+        """
+        pass
+
+    def embed_documents(self, kb: str, documents: List[str], metadatas: List[dict] = None, ids: List[str] = None, hashes: List[str] = None, collection: str = "base", compute_metadata: bool = False) -> None:
+        """
+        Method for embedding documents.
+        :param kb: Target knowledgebase.
+        :param documents: Documents to embed.
+        :param metadatas: Metadata entries for documents.
+            Defaults to None.
+        :param ids: Custom IDs to add. 
+            Defaults to the hash of the document contents.
+        :param hashes: Content hashes.
+            Defaults to None in which case hashes are computet.
+        :param collection: Target collection.
+            Defaults to "base".
+        :param compute_metadata: Flag for declaring, whether to compute metadata.
+            Defaults to False.
+        """
+        hashes = [hash_text_with_sha256(document.page_content)
+                  for document in documents] if hashes is None else hashes
+        for doc_index, hash in enumerate(hashes):
+            if hash not in self.documents:
+                path = os.path.join(self.document_directory, f"{hash}.bin")
+                open(os.path.join(self.document_directory, f"{hash}.bin"), "wb").write(
+                    documents[doc_index].encode("utf-8"))
+                self.documents[hash] = {
+                } if metadatas is None else metadatas[doc_index]
+                self.documents[hash]["controller_library_path"] = path
+
+        if metadatas is None:
+            metadatas = [self.documents[hash] for hash in hashes]
+
+        self.kbs[kb].embed_documents(
+            collection=collection, documents=documents, metadatas=metadatas, ids=hashes if ids is None else ids)
 
     """
     Gateway methods
